ef381829d78af86a932048c757884db896ce5e1e
Sean Golden <S00154367@mail.itsligo.ie>
2020-06-08 17:40:45 +0100

    Homebrew Monsters Added

---
+++ Homebrew Monster List.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MosterGenWPF
+{
+    class Homebrew_Monster_List
+    {
+        public Homebrew_Monster_List()
+        {
+            List<Monster> MonsterList = new List<Monster>();
+            MonsterList.Add(new Monster("17", "Abysswalker", "Homebrew", "0"));
+            MonsterList.Add(new Monster("12", "Alpha Minotaur", "Homebrew", "0"));
+            MonsterList.Add(new Monster("9", "Baku", "Homebrew", "0"));
+            MonsterList.Add(new Monster("14", "Balewing", "Homebrew", "0"));
+            MonsterList.Add(new Monster("16", "Black Root", "Homebrew", "0"));
+            MonsterList.Add(new Monster("17", "Brachydios", "Homebrew", "0"));
+            MonsterList.Add(new Monster("16", "Cenokian Wraith", "Homebrew", "0"));
+            MonsterList.Add(new Monster("14", "Cerynitis", "Homebrew", "0"));
+            MonsterList.Add(new Monster("15", "Chosen of Baphomet", "Homebrew", "0"));
+            MonsterList.Add(new Monster("17", "Colossal Undead", "Homebrew", "0"));
+            MonsterList.Add(new Monster("17", "Crypt Horror", "Homebrew", "0"));
+            MonsterList.Add(new Monster("18", "Crystalline Dragon", "Homebrew", "0"));
+            MonsterList.Add(new Monster("15", "Dolgotha", "Homebrew", "0"));
+            MonsterList.Add(new Monster("15", "Fusion Elemental", "Homebrew", "0"));
+            MonsterList.Add(new Monster("16", "Gargolem", "Homebrew", "0"));
+            MonsterList.Add(new Monster("19", "Ghoul Regent", "Homebrew", "0"));
+            MonsterList.Add(new Monster("30", "Godzilla", "Homebrew", "0"));
+            MonsterList.Add(new Monster("10", "Greater Basilisk", "Homebrew", "0"));
+            MonsterList.Add(new Monster("14", "Green Enchantress", "Homebrew", "0"));
+            MonsterList.Add(new Monster("16", "Implacable Assassin", "Homebrew", "0"));
+            MonsterList.Add(new Monster("13", "Krampus", "Homebrew", "0"));
+            MonsterList.Add(new Monster("17", "Megalodon", "Homebrew", "0"));
+            MonsterList.Add(new Monster("14", "Morvudd", "Homebrew", "0"));
+            MonsterList.Add(new Monster("14", "Mula", "Homebrew", "0"));
+            MonsterList.Add(new Monster("10", "Nightblade", "Homebrew", "0"));
+            MonsterList.Add(new Monster("15", "Pesta", "Homebrew", "0"));
+            MonsterList.Add(new Monster("15", "Plated Behir", "Homebrew", "0"));
+            MonsterList.Add(new Monster("14", "Royal Griffin", "Homebrew", "0"));
+            MonsterList.Add(new Monster("1", "Sea Rex", "Homebrew", "0"));
+            MonsterList.Add(new Monster("15", "Shadow Dragon", "Homebrew", "0"));
+            MonsterList.Add(new Monster("12", "Soul of Ember", "Homebrew", "0"));
+            MonsterList.Add(new Monster("15", "Spawn of Orcus", "Homebrew", "0"));
+            MonsterList.Add(new Monster("11", "Spriggan", "Homebrew", "0"));
+            MonsterList.Add(new Monster("10", "The Rock Golem", "Homebrew", "0"));
+            MonsterList.Add(new Monster("20", "Thunderbird", "Homebrew", "0"));
+            MonsterList.Add(new Monster("12", "Tormentor", "Homebrew", "0"));
+            MonsterList.Add(new Monster("16", "Ulfhedinn", "Homebrew", "0"));
+            MonsterList.Add(new Monster("19", "Varghulf", "Homebrew", "0"));
+            MonsterList.Add(new Monster("16", "War Titan", "Homebrew", "0"));
+            MonsterList.Add(new Monster("17", "Wild Hunt General", "Homebrew", "0"));
+
+
+
+        }
+    }
+}
